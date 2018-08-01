d9b98748856e8e2689fe15e67e80962a87a2f76a
Raphael Cohn <raphael.cohn@stormmq.com>
2018-08-01 18:02:34 +0100

    Optimized

--- src/tree_bitmap/mod.rs
+++ src/tree_bitmap/mod.rs
@@ -12,7 +12,7 @@ use self::allocator::{Allocator, AllocatorHandle};
 use self::node::{MatchResult, Node};
 use std::ptr;
 
-// #[derive(Debug)]
+#[derive(Debug)]
 pub struct TreeBitmap<T: Sized> {
     trienodes: Allocator<Node>,
     results: Allocator<T>,
@@ -147,6 +147,42 @@ impl<T: Sized> TreeBitmap<T> {
             }
             None => None,
         }
+    }
+    
+    /// longest match lookup of ```nibbles```. Returns bits matched as u32, and reference to T.
+    #[inline(always)]
+    pub fn longest_match_present(&self, nibbles: &[u8]) -> bool {
+        let mut cur_hdl = self.root_handle();
+        let mut cur_index = 0;
+        let mut best_match = false;
+        
+        for nibble in nibbles {
+            let cur_node = *self.trienodes.get(&cur_hdl, cur_index);
+            let match_mask = unsafe { *node::MATCH_MASKS.get_unchecked(*nibble as usize) };
+            
+            if let MatchResult::Match(_result_hdl, _result_index, _matching_bit_index) =
+            cur_node.match_internal(match_mask)
+            {
+                best_match = true;
+            }
+            
+            if cur_node.is_endnode() {
+                break;
+            }
+            match cur_node.match_external(match_mask) {
+                MatchResult::Chase(child_hdl, child_index) => {
+                    cur_hdl = child_hdl;
+                    cur_index = child_index;
+                    continue;
+                }
+                MatchResult::None => {
+                    break;
+                }
+                _ => unreachable!(),
+            }
+        }
+		
+		best_match
     }
 
     #[inline(always)]
